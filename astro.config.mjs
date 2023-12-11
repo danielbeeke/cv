ec5a76a976ce95a55fa6fc9be8fc41df1ed04b3d
Daniel Beeke <mail@danielbeeke.nl>
2023-12-11 17:55:24 +0100

    pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
 import AstroPWA from '@vite-pwa/astro'
+import manifest from './src/manifest.json' assert { type: 'json'}
 
 // https://astro.build/config
 export default defineConfig({
-  // integrations: [AstroPWA()],
+  integrations: [AstroPWA({
+    registerType: 'autoUpdate',
+    manifest,
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    },
+    devOptions: {
+      enabled: true
+    }
+  })],
   site: 'https://cv.danielbeeke.nl',
 })
